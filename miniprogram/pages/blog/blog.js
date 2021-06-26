c4fbd59ed07938ed5c61f791b78ff5f6dda2db07
刘健 <liujian@liujiandeMacBook-Pro.local>
2021-06-27 02:15:20 +0800

    feat:1.0.3 改版

--- miniprogram/pages/blog/blog.js
+++ miniprogram/pages/blog/blog.js
@@ -5,7 +5,59 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    iconList: [{
+      icon: 'cardboardfill',
+      color: 'red',
+      badge: 120,
+      name: '活动'
+    }, {
+      icon: 'recordfill',
+      color: 'orange',
+      badge: 1,
+      name: '技术'
+    }, {
+      icon: 'picfill',
+      color: 'yellow',
+      badge: 0,
+      name: '业务'
+    }, {
+      icon: 'noticefill',
+      color: 'olive',
+      badge: 22,
+      name: '消息'
+    }, {
+      icon: 'upstagefill',
+      color: 'cyan',
+      badge: 0,
+      name: '同学'
+    }, {
+      icon: 'clothesfill',
+      color: 'blue',
+      badge: 0,
+      name: '团队'
+    }, {
+      icon: 'discoverfill',
+      color: 'purple',
+      badge: 0,
+      name: '技术'
+    }, {
+      icon: 'questionfill',
+      color: 'mauve',
+      badge: 0,
+      name: '帮助'
+    }, {
+      icon: 'commandfill',
+      color: 'purple',
+      badge: 0,
+      name: '问答'
+    }, {
+      icon: 'brandfill',
+      color: 'mauve',
+      badge: 0,
+      name: '版权'
+    }],
+    gridCol:3,
+    skin: false
   },
   // 打开官网
   openWebsite :function(){
--- miniprogram/pages/playlist/playlist.js
+++ miniprogram/pages/playlist/playlist.js
@@ -1,10 +1,19 @@
 // pages/playlist/playlist.js
+
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    motto: 'Hi 开发者！',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    activeName: '1',
     openid:'',
     swiperImageUrl:[
       {url: 'https://gw.alipayobjects.com/mdn/rms_ed9d16/afts/img/A*2z-FT7Tqv9YAAAAAAAAAAAAAARQnAQ'}
@@ -27,17 +36,68 @@ Page({
       }]
   },
 
+  onChange(event) {
+    this.setData({
+      activeName: event.detail,
+    });
+  },
+
+    //事件处理函数
+    bindViewTap: function() {
+      wx.navigateTo({
+        url: '../logs/logs'
+      })
+    },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    wx.cloud.callFunction({
-      name:'login',
-    }).then(data=>{
-      console.log(data)
+  // onLoad: function (options) {
+  //   wx.cloud.callFunction({
+  //     name:'login',
+  //   }).then(data=>{
+  //     console.log(data)
+  //     this.setData({
+  //       openid:data.result.openid
+  //     })
+  //   })
+  // },
+
+  onLoad: function () {
+    console.log('app.globalData.userInfo',app.globalData.userInfo,this.data.canIUse,this.data.hasUserInfo,!hasUserInfo && canIUse)
+    if (app.globalData.userInfo) {
       this.setData({
-        openid:data.result.openid
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
       })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   },
 
